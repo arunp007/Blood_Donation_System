8d6cd529cb5e766a3f20882996e5ba4ecc2b74b8
arunp007 <arunp4624@gmail.com>
2022-05-08 16:15:10 +0530

    Main Home Page Completed

--- myproject/mainhomeapp/urls.py
+++ myproject/mainhomeapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('main',views.main)
+    path('main',views.main, name = "main"),
+    path('about',views.about, name = "about"),
+    path('contact',views.contact, name = "contact")
 ]
--- myproject/mainhomeapp/views.py
+++ myproject/mainhomeapp/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'mainhome.html')
+
+def about(request):
+    return render(request, 'aboutus.html')
+
+def contact(request):
+    return render(request, 'contactus.html')
