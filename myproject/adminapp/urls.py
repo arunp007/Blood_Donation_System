f5e47e176d811ae645def7ef439a81a7ea79439b
arunp007 <arunp4624@gmail.com>
2022-06-07 13:03:19 +0530

    Mobile View Fixed

--- myproject/adminapp/urls.py
+++ myproject/adminapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('admin',views.admin)
+    path('admin/',views.admin, name='admin_home'),
+    path('adminnotification/',views.adminnotification, name='admin_notification')
 ]
--- myproject/adminapp/views.py
+++ myproject/adminapp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def admin(request):
     return render(request, 'adminpage.html')
+
+def adminnotification(request):
+    return render(request, 'admin_notification.html')
