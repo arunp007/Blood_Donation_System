d43e0b9417f56e2048e25a5b210aeab487668b7f
arunp007 <arunp4624@gmail.com>
2022-07-05 14:22:12 +0530

    Crud Added

--- myproject/hospitalapp/urls.py
+++ myproject/hospitalapp/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('hospital',views.hospital,name="hospital_home"),
-    path('hospitalsearch',views.hospitalsearch,name="hospital_search"),
-    path('hospitalnotification',views.hospitalnotification,name="hospitalnotification"),
-    path('hospitalcamp',views.hospitalcamp,name='hospitalcamp'),
-    path('camptable',views.camptable, name="camptable"),
+    path('hospital', views.hospital, name="hospital_home"),
+    path('hospitalsearch', views.hospitalsearch, name="hospital_search"),
+    path('hospitalnotification', views.hospitalnotification, name="hospitalnotification"),
+    path('hospitalcamp', views.hospitalcamp, name='hospitalcamp'),
+    path('camptable', views.camptable, name="camptable"),
+    path('camp_details', views.camp_details, name = 'camp_details'),
+    path('camp', views.camp, name = 'camp'),
+    path('camp_update<int:id>', views.camp_update, name = 'camp_update'),
+    path('camp_delete<int:id>', views.camp_delete, name = 'camp_delete')
   
 ]
--- myproject/hospitalapp/views.py
+++ myproject/hospitalapp/views.py
@@ -1,5 +1,4 @@
-from urllib.request import Request
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from.models import *
 
 def hospital(request):
@@ -21,10 +20,50 @@ def hospitalcamp(request):
         name = request.POST['name']
         phone = request.POST['phone']
         email = request.POST['email']
-        details = Camps(camp = camp,date = date,address = address,state = state,district = district,name = name,phone = phone,email = email)
+        details = Camps(camp = camp, date = date, address = address, state = state, district = district, name = name, phone = phone, email = email)
         details.save()
     return render(request, 'hospital_camp.html')
 
 def camptable(request):
     camp_details = Camps.objects.all()
     return render(request, 'camptable.html', {'details' : camp_details})
+
+def camp_details(request):
+    camp_data = Camps.objects.all()
+    return render(request, 'camp_details.html', {'camp': camp_data})
+
+def camp(request):
+    if request.method == 'POST':
+        camp = request.POST['camp']
+        date = request.POST['date']
+        address = request.POST['address']
+        state = request.POST['state']
+        district = request.POST['district']
+        name = request.POST['name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        details = Camps(camp = camp, date = date, address = address, state = state, district = district, name = name, phone = phone, email = email)
+        details.save()
+    return render(request, 'camp.html')
+
+def camp_update(request,id):
+    if request.method == 'POST':
+        update_data = ""
+        camp = request.POST['camp']
+        date = request.POST['date']
+        address = request.POST['address']
+        state = request.POST['state']
+        district = request.POST['district']
+        name = request.POST['name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        Camps.objects.filter(id = id).update(camp = camp, date = date, address = address, state = state, district = district, name = name, phone = phone, email = email)
+        return redirect('camp_details')
+
+    else:
+        update_data = Camps.objects.get(id = id)
+        return render(request, 'camp.html', {'update': update_data})
+
+def camp_delete(request,id):
+    Camps.objects.get(id = id).delete()
+    return redirect('camp_details')
