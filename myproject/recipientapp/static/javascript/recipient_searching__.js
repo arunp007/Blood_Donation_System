65f41531fe78636af25deba98cb51fb6480c1d9b
arunp007 <arunp4624@gmail.com>
2022-10-06 21:13:17 +0530

    Added Some Changes

---
+++ myproject/recipientapp/static/javascript/recipient_searching__.js
@@ -0,0 +1,198 @@
+function search(){
+    var blood = document.getElementById('bloodgroup')
+    var location = document.getElementById('location')
+
+    if(blood.value == 'A+' && location.value == 'Karuvarakundu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/kvk_aplus")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Karuvarakundu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/kvk_aminus")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Karuvarakundu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/kvk_bplus")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Karuvarakundu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/kvk_bminus")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Karuvarakundu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/kvk_oplus")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Karuvarakundu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/kvk_ominus")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Karuvarakundu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/kvk_abplus")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Karuvarakundu'){
+        document.getElementById('table_error').innerHTML="No Data Founded"
+        return false
+    }
+
+    if(blood.value == 'A+' && location.value == 'Manjeri'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/manjeri_aplus")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Manjeri'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/manjeri_aminus")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Manjeri'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/manjeri_bplus")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Manjeri'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/manjeri_bminus")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Manjeri'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recpientapp/manjeri_oplus")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Manjeri'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/manjeri_ominus")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Manjeri'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/manjeri_abplus")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Manjeri'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/manjeri_abminus")
+    }
+
+    if(blood.value == 'A+' && location.value == 'Malappuram'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/mal_aplus")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Malappuram'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/mal_aminus")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Malappuram'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/mal_bplus")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Malappuram'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/mal_bminus")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Malappuram'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/mal_oplus")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Malappuram'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/mal_ominus")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Malappuram'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/mal_abplus")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Malappuram'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/mal_abminus")
+    }
+
+    if(blood.value == 'A+' && location.value == 'Nilambur'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/n_aplus")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Nilambur'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/n_aminus")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Nilambur'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/n_bplus")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Nilambur'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/n_bminus")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Nilambur'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/n_oplus")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Nilambur'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/n_ominus")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Nilambur'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/n_abplus")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Nilambur'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/n_abminus")
+    }
+
+    if(blood.value == 'A+' && location.value == 'Wandoor'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/w_aplus")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Wandoor'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/w_aminus")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Wandoor'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/w_bplus")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Wandoor'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/w_bminus")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Wandoor'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/w_oplus")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Wandoor'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/w_ominus")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Wandoor'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/w_abplus")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Wandoor'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/w_abminus")
+    }
+
+    if(blood.value == 'A+' && location.value == 'Vazhikkadavu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/v_aplus")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Vazhikkadavu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/v_aminus")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Vazhikkadavu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/v_bplus")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Vazhikkadavu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/v_bminus")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Vazhikkadavu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/v_oplus")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Vazhikkadavu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/v_ominus")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Vazhikkadavu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/v_abplus")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Vazhikkadavu'){
+        window.open("https://blooddonationsystem1.herokuapp.com/recipientapp/v_abminus")
+    }
+    
+}
