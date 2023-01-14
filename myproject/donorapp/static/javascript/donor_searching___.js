434e1538a2eb2c522e2cf6fba0eceeac99255b3c
arunp007 <arunp4624@gmail.com>
2023-01-14 21:50:00 +0530

    Added Some Changes

---
+++ myproject/donorapp/static/javascript/donor_searching___.js
@@ -0,0 +1,198 @@
+function search(){
+    var blood = document.getElementById('bloodgroup')
+    var location = document.getElementById('location')
+
+    if(blood.value == 'A+' && location.value == 'Karuvarakundu'){
+        window.open("https://donor123.w3spaces.com/kvk_aplus.html")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Karuvarakundu'){
+        window.open("https://donor123.w3spaces.com/kvk_aminus.html")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Karuvarakundu'){
+        window.open("https://donor123.w3spaces.com/kvk_bplus.html")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Karuvarakundu'){
+        window.open("https://donor123.w3spaces.com/kvk_bminus.html")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Karuvarakundu'){
+        window.open("https://donor123.w3spaces.com/kvk_oplus.html")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Karuvarakundu'){
+        window.open("https://donor123.w3spaces.com/kvk_ominus.html")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Karuvarakundu'){
+        window.open("https://donor123.w3spaces.com/kvk_abplus.html")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Karuvarakundu'){
+        window.open("https://donor123.w3spaces.com/kvk_abminus.html")
+    }
+
+    if(blood.value == 'A+' && location.value == 'Manjeri'){
+        window.open("https://arun955.w3spaces.com/manjeri_aplus.html")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Manjeri'){
+        window.open("https://arun955.w3spaces.com/manjeri_aminus.html")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Manjeri'){
+        window.open("https://arun955.w3spaces.com/manjeri_bplus.html")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Manjeri'){
+        window.open("https://arun955.w3spaces.com/manjeri_bminus.html")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Manjeri'){
+        window.open("https://arun955.w3spaces.com/manjeri_oplus.html")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Manjeri'){
+        window.open("https://arun955.w3spaces.com/manjeri_ominus.html")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Manjeri'){
+        window.open("https://arun955.w3spaces.com/manjeri_abplus.html")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Manjeri'){
+        window.open("https://arun955.w3spaces.com/manjeri_abminus.html")
+    }
+
+    if(blood.value == 'A+' && location.value == 'Malappuram'){
+        window.open("https://codemaster8848.w3spaces.com/mal_aplus.html")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Malappuram'){
+        window.open("https://codemaster8848.w3spaces.com/mal_aminus.html")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Malappuram'){
+        window.open("https://codemaster8848.w3spaces.com/mal_bplus.html")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Malappuram'){
+        window.open("https://codemaster8848.w3spaces.com/mal_bminus.html")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Malappuram'){
+        window.open("https://codemaster8848.w3spaces.com/mal_oplus.html")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Malappuram'){
+        window.open("https://codemaster8848.w3spaces.com/mal_ominus.html")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Malappuram'){
+        window.open("https://arun955.w3spaces.com/mal_abplus.html")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Malappuram'){
+        window.open("https://codemaster8848.w3spaces.com/mal_abminus.html")
+    }
+
+    if(blood.value == 'A+' && location.value == 'Nilambur'){
+        window.open("https://arun25648.w3spaces.com/nlbr_aplus.html")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Nilambur'){
+        window.open("https://arun25648.w3spaces.com/nlbr_aminus.html")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Nilambur'){
+        window.open("https://arun25648.w3spaces.com/nlbr_bplus.html")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Nilambur'){
+        window.open("https://arun25648.w3spaces.com/nlbr_bminus.html")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Nilambur'){
+        window.open("https://arun25648.w3spaces.com/nlbr_oplus.html")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Nilambur'){
+        window.open("https://arun25648.w3spaces.com/nlbr_ominus.html")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Nilambur'){
+        window.open("https://arun25648.w3spaces.com/nlbr_abplus.html")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Nilambur'){
+        window.open("https://arun25648.w3spaces.com/nlbr_abminus.html")
+    }
+
+    if(blood.value == 'A+' && location.value == 'Wandoor'){
+        window.open("https://lifesaverdonor123.w3spaces.com/wandoor_aplus.html")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Wandoor'){
+        window.open("https://lifesaverdonor123.w3spaces.com/wandoor_aminus.html")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Wandoor'){
+        window.open("https://lifesaverdonor123.w3spaces.com/wandoor_bplus.html")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Wandoor'){
+        window.open("https://lifesaverdonor123.w3spaces.com/wandoor_bminus.html")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Wandoor'){
+        window.open("https://lifesaverdonor123.w3spaces.com/wandoor_oplus.html")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Wandoor'){
+        window.open("https://lifesaverdonor123.w3spaces.com/wandoor_ominus.html")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Wandoor'){
+        window.open("https://lifesaverdonor123.w3spaces.com/wandoor_abplus.html")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Wandoor'){
+        window.open("https://lifesaverdonor123.w3spaces.com/wandoor_abminus.html")
+    }
+
+    if(blood.value == 'A+' && location.value == 'Vazhikkadavu'){
+        window.open("https://lifesaver1345.w3spaces.com/v_aplus.html")
+    }
+
+    if(blood.value == 'A-' && location.value == 'Vazhikkadavu'){
+        window.open("https://lifesaver1345.w3spaces.com/v_aminus.html")
+    }
+
+    if(blood.value == 'B+' && location.value == 'Vazhikkadavu'){
+        window.open("https://lifesaver1345.w3spaces.com/v_bplus.html")
+    }
+
+    if(blood.value == 'B-' && location.value == 'Vazhikkadavu'){
+        window.open("https://lifesaver1345.w3spaces.com/v_bminus.html")
+    }
+
+    if(blood.value == 'O+' && location.value == 'Vazhikkadavu'){
+        window.open("https://lifesaver1345.w3spaces.com/v_oplus.html")
+    }
+
+    if(blood.value == 'O-' && location.value == 'Vazhikkadavu'){
+        window.open("https://lifesaver1345.w3spaces.com/v_ominus.html")
+    }
+
+    if(blood.value == 'AB+' && location.value == 'Vazhikkadavu'){
+        window.open("https://lifesaver1345.w3spaces.com/v_abplus.html")
+    }
+
+    if(blood.value == 'AB-' && location.value == 'Vazhikkadavu'){
+        window.open("https://lifesaver1345.w3spaces.com/v_abminus.html")
+    }
+    
+}
+
